27015b34dd4fafc8d4d38b11d5f8c1f780a30676
kshg1324(laptop) <kshg1324@naver.com>
2018-09-17 14:56:45 +0900

    Drill#5 1번째 단계
    지정된 x,y 좌표로 이동하는 함수 원형을 만듬

---
+++ Lecture04/#5.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+def move_to_destination(x, y):
+    pass
+
+while True:
+    move_to_destination(0, 0)
+
+close_canvas()
