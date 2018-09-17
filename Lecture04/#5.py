e0d1eaa2442b80ebfd3d14affb7efaea6ece88e3
kshg1324(laptop) <kshg1324@naver.com>
2018-09-17 16:23:13 +0900

    Drill#5 8번째단계
    더블버퍼링이 되어있지 않았기 때문에 깜빡거림이 있었음.  clear_canvas_now() 함수를
     clear_canvas()로, grass.draw_now(400, 30) 함수를 grass.draw(400, 30) 함수를 바꿔준 후 get_events()함수를 사용하여 깜빡거림을 없애줌.

--- Lecture04/#5.py
+++ Lecture04/#5.py
@@ -12,40 +12,44 @@ def move_to_destination(x, y):
     global Cx, Cy
 
     while (x > Cx):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
+        clear_canvas()
+        grass.draw(400, 30)
         character.clip_draw(frame * 100, 100, 100, 100, Cx, Cy)
         update_canvas()
         frame = (frame + 1) % 8
         Cx += 1
         delay(0.01)
+        get_events()
 
     while (y > Cy):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
+        clear_canvas()
+        grass.draw(400, 30)
         character.clip_draw(frame * 100, 100, 100, 100, Cx, Cy)
         update_canvas()
         frame = (frame + 1) % 8
         Cy += 1
         delay(0.01)
+        get_events()
 
     while (x < Cx):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
+        clear_canvas()
+        grass.draw(400, 30)
         character.clip_draw(frame * 100, 0, 100, 100, Cx, Cy)
         update_canvas()
         frame = (frame + 1) % 8
         Cx -= 1
         delay(0.01)
+        get_events()
 
     while (y < Cy):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
+        clear_canvas()
+        grass.draw(400, 30)
         character.clip_draw(frame * 100, 0, 100, 100, Cx, Cy)
         update_canvas()
         frame = (frame + 1) % 8
         Cy -= 1
         delay(0.01)
+        get_events()
 
 
 while True:
