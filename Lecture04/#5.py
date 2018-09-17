973152794221deefdc2a7e6abb44daa00d1ab74b
kshg1324(laptop) <kshg1324@naver.com>
2018-09-17 15:13:12 +0900

    Drill#5 3번째단계
    첫번째 좌표의 y좌표까지 따라가게 해봄

--- Lecture04/#5.py
+++ Lecture04/#5.py
@@ -11,10 +11,17 @@ def move_to_destination(x, y):
     while (x > Cx):
         clear_canvas_now()
         grass.draw_now(400, 30)
-
         character.clip_draw(frame * 100, 100, 100, 100, Cx, Cy)
         update_canvas()
         Cx += 1
+        delay(0.01)
+
+    while (y > Cy):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, Cx, Cy)
+        update_canvas()
+        Cy += 1
         delay(0.01)
 
 
