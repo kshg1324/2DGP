a3e26b932131ad088052128c046aa49a049339b1
kshg1324(laptop) <kshg1324@naver.com>
2018-10-02 16:44:06 +0900

    DRILL#7 2번째 커밋
    캐릭터를 화면에 띄워봄

--- Lecture06/character_line.py
+++ Lecture06/character_line.py
@@ -5,9 +5,12 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 KPU_GROUND = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
+frame = 0
 while True:
     clear_canvas()
     KPU_GROUND.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100, 100, 100, 100, 100)
+    frame = (frame + 1) % 8
     update_canvas()
     get_events()
 
