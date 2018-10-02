a22764f87f66df97ffcb9c604eeb183eaa02909e
kshg1324(laptop) <kshg1324@naver.com>
2018-10-02 16:53:55 +0900

    DRILL#7 3번째 커밋
    기본적인 직선을 긋는 함수를 짜봄
    캐릭터를 그리는 코드를 함수안에 집어넣음

--- Lecture06/character_line.py
+++ Lecture06/character_line.py
@@ -6,11 +6,27 @@ KPU_GROUND = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 frame = 0
+size = 20
+
+points = [(random.randint(0, 1280), random.randint(0, 1080)) for i in range(size)]
+
+def draw_line():
+    global frame
+    for i in range(0, 100 + 1, 2):
+        t = i / 100
+        x = (1 - t) * (points[0])[0] + t * (points[1])[0]
+        y = (1 - t) * (points[0])[1] + t * (points[1])[1]
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        frame = (frame + 1) % 8
+        delay(0.05)
+        update_canvas()
+        get_events()
+
+
 while True:
     clear_canvas()
     KPU_GROUND.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100, 100, 100, 100, 100)
-    frame = (frame + 1) % 8
+    draw_line()
     update_canvas()
     get_events()
 
