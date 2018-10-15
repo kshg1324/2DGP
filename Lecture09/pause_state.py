ede2015e5a5433be8a2b5f9dc6fad01aa34b04da
kshg1324(laptop) <kshg1324@naver.com>
2018-10-15 22:29:38 +0900

    DRILL#10-1 1번째 커밋
    2015182003 권호민 DRILL10-1 1번째 커밋
    main_state의 pause()함수에서 처리해보려고 했는데 안되는 것 같아서 pause_state라는 state를 추가했음

---
+++ Lecture09/pause_state.py
@@ -0,0 +1,65 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+import main_state
+
+
+
+name = "PauseState"
+
+boy = None
+grass = None
+font = None
+
+
+
+class Pause:
+    def __init__(self):
+        self.image = load_image('pause.png')
+
+    def draw(self):
+        self.image.draw(800, 600)
+
+def enter():
+    global pause
+    pause = Pause()
+
+def exit():
+    global pause
+    del(pause)
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            game_framework.change_state(main_state)
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+    pause.draw()
+    update_canvas()
+
+
+
+
+
