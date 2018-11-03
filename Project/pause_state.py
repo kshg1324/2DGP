23af645bfd7f6863c13c9cf139622c804c6be02e
kshg1324(laptop) <kshg1324@naver.com>
2018-11-04 02:04:03 +0900

    게임 시작 전 메뉴
    시작 및 종료 추가

---
+++ Project/pause_state.py
@@ -0,0 +1,71 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+import main_state
+
+
+
+name = "PauseState"
+
+boy = None
+grass = None
+font = None
+
+
+
+class Pause:
+    def __init__(self):
+        self.count = 0
+        self.image = load_image('pause.png')
+    def update(self):
+        self.count = (self.count + 1) % 100
+    def draw(self):
+        if(self.count <= 50):
+            self.image.draw(400, 300)
+
+def enter():
+    global pause
+    pause = Pause()
+
+def exit():
+    global pause
+    del(pause)
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            game_framework.pop_state()
+
+
+def update():
+    pause.update()
+
+
+def draw():
+    clear_canvas()
+    main_state.grass.draw()
+    main_state.boy.draw()
+    pause.draw()
+    update_canvas()
+
+
+
+
+
---
+++ Project/start_state.py
@@ -0,0 +1,70 @@
+import game_framework
+import main_state
+from pico2d import *
+
+
+name = "StartState"
+image = None
+image2 = None
+image3 = None
+CX = 0
+CY = 0
+
+def enter():
+    global image, image2, image3
+    image = load_image('title.png')
+    image2 = load_image('game_start.png')
+    image3 = load_image('game_quit.png')
+
+
+def exit():
+    global image, image2, image3
+    del(image)
+    del (image2)
+    del (image3)
+
+def handle_events():
+    events = get_events()
+    global CX, CY
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_MOUSEMOTION:
+            CX, CY = event.x, 600 - 1 - event.y
+            print("CX =", CX)
+            print("CY =", CY)
+        else:
+            if(event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            if event.type == SDL_MOUSEBUTTONDOWN:
+                if (350 <= CX <= 450 and 285 <= CY <= 315):
+                    game_framework.change_state(main_state)
+            if event.type == SDL_MOUSEBUTTONDOWN:
+                if (350 <= CX <= 450 and 235 <= CY <= 265):
+                    game_framework.quit()
+
+
+def draw():
+    clear_canvas()
+    image.draw(400 ,300)
+    image2.draw(400,300)
+    image3.draw(400,250)
+    update_canvas()
+
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+
+
+
+
