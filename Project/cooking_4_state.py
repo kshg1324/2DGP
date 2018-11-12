f407c9964e4b075ac87ea2924b65ba150637052c
kshg1324(laptop) <kshg1324@naver.com>
2018-11-12 17:18:57 +0900

    음식 제작 구현
    6가지 모두 구현

--- Project/cooking_4_state.py
+++ Project/cooking_4_state.py
@@ -6,10 +6,10 @@ import game_framework
 import main_state
 from pico2d import *
 
-name = "CookingState"
+name = "Cooking_4_State"
 font = None
-cook_stack_1 = 0
-class Cooking:
+cook_stack_4 = 0
+class Cooking_4:
     def __init__(self):
         self.image1 = load_image('cooking_background.png')
         self.image2 = load_image('menu.png')
@@ -17,33 +17,31 @@ class Cooking:
     def update(self):
         pass
     def draw(self):
-        global cook_stack_1
+        global cook_stack_4
         self.image1.draw(400, 300)
-        self.image2.clip_draw(100 * 0, 100 * 6, 100, 100 , 50 + 400 + 100 * -3, 400)
+        #self.image2.clip_draw(100 * 0, 100 * 6, 100, 100 , 50 + 400 + 100 * -3, 400)
         # self.image2.clip_draw(100 * 0, 100 * 0, 100, 100 , 50 + 400+ 100 * -2, 400)
         # self.image2.clip_draw(100 * 5, 100 * 4, 100, 100 , 50 + 400+ 100 * -1, 400)
-        # self.image2.clip_draw(100 * 2, 100 * 3, 100, 100 , 50 + 400+ 100 * 0, 400)
+        self.image2.clip_draw(100 * 2, 100 * 3, 100, 100 , 50 + 400+ 100 * 0, 400)
         # self.image2.clip_draw(100 * 2, 100 * 5, 100, 100 , 50 + 400+ 100 * 1, 400)
         # self.image2.clip_draw(100 * 0, 100 * 0, 100, 100 , 50 + 400+ 100 * 2, 400)
-        if cook_stack_1 == 0:
-            self.font.draw(400, 300, '(o m e l e t)', (0, 0, 0))
-        elif cook_stack_1 == 1:
-            self.font.draw(400, 300, '(m e l e t)', (0, 0, 0))
-        elif cook_stack_1 == 2:
-            self.font.draw(400, 300, '(e l e t)', (0, 0, 0))
-        elif cook_stack_1 == 3:
-            self.font.draw(400, 300, '(l e t)', (0, 0, 0))
-        elif cook_stack_1 == 4:
-            self.font.draw(400, 300, '(e t)', (0, 0, 0))
-        elif cook_stack_1 == 5:
-            self.font.draw(400, 300, '(t)', (0, 0, 0))
+        if cook_stack_4 == 0:
+            self.font.draw(400, 300, '(p i z z a)', (0, 0, 0))
+        elif cook_stack_4 == 1:
+            self.font.draw(400, 300, '(i z z a)', (0, 0, 0))
+        elif cook_stack_4 == 2:
+            self.font.draw(400, 300, '(z z a)', (0, 0, 0))
+        elif cook_stack_4 == 3:
+            self.font.draw(400, 300, '(z a)', (0, 0, 0))
+        elif cook_stack_4 == 4:
+            self.font.draw(400, 300, '(a)', (0, 0, 0))
 def enter():
-    global cooking
-    cooking = Cooking()
+    global cooking_4
+    cooking_4 = Cooking_4()
 
 def exit():
-    global cooking
-    del(cooking)
+    global cooking_4
+    del(cooking_4)
 
 
 def pause():
@@ -56,24 +54,22 @@ def resume():
 
 
 def handle_events():
-    global cook_stack_1
+    global cook_stack_4
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_o):
-            cook_stack_1+=1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_m) and cook_stack_1 == 1:
-            cook_stack_1 += 1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_e)and cook_stack_1 == 2:
-            cook_stack_1 += 1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_l)and cook_stack_1 == 3:
-            cook_stack_1 += 1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_e)and cook_stack_1 == 4:
-            cook_stack_1 += 1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_t)and cook_stack_1 == 5:
-            main_state.food_1_stack += 1
-            cook_stack_1 = 0
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            cook_stack_4+=1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_i) and cook_stack_4 == 1:
+            cook_stack_4 += 1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_z)and cook_stack_4 == 2:
+            cook_stack_4 += 1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_z)and cook_stack_4 == 3:
+            cook_stack_4 += 1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_a)and cook_stack_4 == 4:
+            main_state.food_4_stack += 1
+            cook_stack_4 = 0
             game_framework.pop_state()
         # elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_w):
         #     main_state.food_2_stack+=1
@@ -91,7 +87,7 @@ def handle_events():
 
 
 def update():
-    cooking.update()
+    cooking_4.update()
     #pass
 
 
@@ -107,5 +103,5 @@ def draw():
     main_state.wall.draw()
     main_state.frame.draw()
     main_state.table.draw()
-    cooking.draw()
+    cooking_4.draw()
     update_canvas()
--- Project/cooking_5_state.py
+++ Project/cooking_5_state.py
@@ -8,8 +8,8 @@ from pico2d import *
 
 name = "CookingState"
 font = None
-cook_stack_1 = 0
-class Cooking:
+cook_stack_5 = 0
+class Cooking_5:
     def __init__(self):
         self.image1 = load_image('cooking_background.png')
         self.image2 = load_image('menu.png')
@@ -17,33 +17,29 @@ class Cooking:
     def update(self):
         pass
     def draw(self):
-        global cook_stack_1
+        global cook_stack_5
         self.image1.draw(400, 300)
-        self.image2.clip_draw(100 * 0, 100 * 6, 100, 100 , 50 + 400 + 100 * -3, 400)
+        #self.image2.clip_draw(100 * 0, 100 * 6, 100, 100 , 50 + 400 + 100 * -3, 400)
         # self.image2.clip_draw(100 * 0, 100 * 0, 100, 100 , 50 + 400+ 100 * -2, 400)
         # self.image2.clip_draw(100 * 5, 100 * 4, 100, 100 , 50 + 400+ 100 * -1, 400)
         # self.image2.clip_draw(100 * 2, 100 * 3, 100, 100 , 50 + 400+ 100 * 0, 400)
-        # self.image2.clip_draw(100 * 2, 100 * 5, 100, 100 , 50 + 400+ 100 * 1, 400)
+        self.image2.clip_draw(100 * 2, 100 * 5, 100, 100 , 50 + 400+ 100 * 1, 400)
         # self.image2.clip_draw(100 * 0, 100 * 0, 100, 100 , 50 + 400+ 100 * 2, 400)
-        if cook_stack_1 == 0:
-            self.font.draw(400, 300, '(o m e l e t)', (0, 0, 0))
-        elif cook_stack_1 == 1:
-            self.font.draw(400, 300, '(m e l e t)', (0, 0, 0))
-        elif cook_stack_1 == 2:
-            self.font.draw(400, 300, '(e l e t)', (0, 0, 0))
-        elif cook_stack_1 == 3:
-            self.font.draw(400, 300, '(l e t)', (0, 0, 0))
-        elif cook_stack_1 == 4:
-            self.font.draw(400, 300, '(e t)', (0, 0, 0))
-        elif cook_stack_1 == 5:
-            self.font.draw(400, 300, '(t)', (0, 0, 0))
+        if cook_stack_5 == 0:
+            self.font.draw(400, 300, '(f i s h)', (0, 0, 0))
+        elif cook_stack_5 == 1:
+            self.font.draw(400, 300, '(i s h)', (0, 0, 0))
+        elif cook_stack_5 == 2:
+            self.font.draw(400, 300, '(s h)', (0, 0, 0))
+        elif cook_stack_5 == 3:
+            self.font.draw(400, 300, '(h)', (0, 0, 0))
 def enter():
-    global cooking
-    cooking = Cooking()
+    global cooking_5
+    cooking_5 = Cooking_5()
 
 def exit():
-    global cooking
-    del(cooking)
+    global cooking_5
+    del(cooking_5)
 
 
 def pause():
@@ -56,24 +52,20 @@ def resume():
 
 
 def handle_events():
-    global cook_stack_1
+    global cook_stack_5
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_o):
-            cook_stack_1+=1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_m) and cook_stack_1 == 1:
-            cook_stack_1 += 1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_e)and cook_stack_1 == 2:
-            cook_stack_1 += 1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_l)and cook_stack_1 == 3:
-            cook_stack_1 += 1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_e)and cook_stack_1 == 4:
-            cook_stack_1 += 1
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_t)and cook_stack_1 == 5:
-            main_state.food_1_stack += 1
-            cook_stack_1 = 0
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_f):
+            cook_stack_5+=1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_i) and cook_stack_5 == 1:
+            cook_stack_5 += 1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_s)and cook_stack_5 == 2:
+            cook_stack_5 += 1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_h)and cook_stack_5 == 3:
+            main_state.food_5_stack += 1
+            cook_stack_5 = 0
             game_framework.pop_state()
         # elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_w):
         #     main_state.food_2_stack+=1
@@ -91,7 +83,7 @@ def handle_events():
 
 
 def update():
-    cooking.update()
+    cooking_5.update()
     #pass
 
 
@@ -107,5 +99,5 @@ def draw():
     main_state.wall.draw()
     main_state.frame.draw()
     main_state.table.draw()
-    cooking.draw()
+    cooking_5.draw()
     update_canvas()
